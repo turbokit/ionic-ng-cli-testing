2fc81de4a9a0055c63bf3c9eeb371a0be95097c5
dev <dev@dev.local>
2017-08-28 16:10:46 +0300

    Init test

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, async } from '@angular/core/testing';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp as AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        IonicModule.forRoot(AppComponent)
+      ],
+      declarations: [
+        AppComponent
+      ],
+    providers: [
+      StatusBar,
+      SplashScreen,
+    ]
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+  // it(`should have as title 'app'`, async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('app');
+  // }));
+
+  // it('should render title in a h1 tag', async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  // }));
+});
